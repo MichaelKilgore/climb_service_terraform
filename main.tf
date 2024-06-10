ebcea0300247dd2f2392a7c90cfb80f9fc95437e
Michael Kilgore <michael@Michaels-MBP-3.lan>
2024-06-09 22:00:50 -0500

    Renaming resources

--- main.tf
+++ main.tf
@@ -30,14 +30,14 @@ resource "google_cloudbuild_trigger" "github-trigger" {
 
 # gives permissions to our github-trigger to execute cloudbuild 
 # releases to pipeline
-resource "google_project_iam_member" "cloudbuild_releaser" {
+resource "google_project_iam_member" "cloudbuild-releaser-permissions" {
   project = var.project_id
   role    = "roles/clouddeploy.releaser"
   member  = "serviceAccount:${var.service_account_number}@cloudbuild.gserviceaccount.com"
 }
 
 # cloud run service (like a lambda)
-resource "google_cloud_run_service" "default" {
+resource "google_cloud_run_service" "beta-cloud-run-service" {
   name     = "beta-cloud-run"
   location = var.default_region
 
@@ -61,7 +61,7 @@ traffic {
   }
 }
 
-resource "google_clouddeploy_target" "primary" {
+resource "google_clouddeploy_target" "beta" {
   location          = var.default_region
   name              = "beta"
   deploy_parameters = {}
@@ -82,33 +82,33 @@ resource "google_clouddeploy_target" "primary" {
 
 
 # deployment pipeline
-resource "google_clouddeploy_delivery_pipeline" "primary" {
+resource "google_clouddeploy_delivery_pipeline" "pipeline" {
   location = var.default_region
   name = "climb-service-pipeline"
   description = "Pipeline to deploy climb service changes."
 
   serial_pipeline {
     stages {
-      target_id = google_clouddeploy_target.primary.target_id
+      target_id = google_clouddeploy_target.beta.target_id
     }
   }
 }
 
-resource "google_service_account_iam_binding" "allow_cloudbuild_to_impersonate" {
+resource "google_service_account_iam_binding" "allow-cloudbuild-to-impersonate-compute-environment" {
   service_account_id = "projects/${var.project_id}/serviceAccounts/${var.service_account_number}-compute@developer.gserviceaccount.com"
   role               = "roles/iam.serviceAccountUser"
   members            = [
     "serviceAccount:${var.service_account_number}@cloudbuild.gserviceaccount.com",
   ]
 }
-resource "google_project_iam_binding" "cloud_build_builder" {
+resource "google_project_iam_binding" "cloud-build-builder" {
   project = var.project_id
   role    = "roles/cloudbuild.builds.builder"
   members = [
     "serviceAccount:${var.service_account_number}@cloudbuild.gserviceaccount.com",
   ]
 }
-resource "google_project_iam_binding" "storage_admin" {
+resource "google_project_iam_binding" "storage-admin" {
   project = var.project_id
   role    = "roles/storage.admin"
   members = [
